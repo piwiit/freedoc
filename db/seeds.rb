ccc0851315b91697a4bfac61ae5bbe8c93e9216d
piwiit <bouillaux@hotmail.fr>
2020-10-23 00:48:54 +0200

    finish cities

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,12 +9,12 @@ require 'faker'
 
 Doctor.destroy_all
 Patient.destroy_all
-# City.destroy_all
+City.destroy_all
 # Specialty.destroy_all
 
 doctors = []
 patients = []
-# cities = Array.new
+cities = []
 # specialties = []
 
 spes = %w[Allergist Anesthesiologist Cardiologist Dermatologist Endocrinologist Gastroenterologist Hematologist Immunologist Internist]
@@ -24,19 +24,19 @@ spes = %w[Allergist Anesthesiologist Cardiologist Dermatologist Endocrinologist 
 #   specialties << spe
 # end
 
-# 10.times do
-#   city = City.create(name: Faker::Address.city)
-#   cities << city
-# end
+10.times do
+  city = City.create(name: Faker::Address.city)
+  cities << city
+end
 
 30.times do
-  doctor = Doctor.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, zip_code: Faker::Address.zip_code, specialty: spes.sample)
-  patient =  Patient.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
+  doctor = Doctor.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, zip_code: Faker::Address.zip_code, specialty: spes.sample, city: cities.sample)
+  patient =  Patient.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, city: cities.sample)
   doctors << doctor
   patients << patient
 end
 
 30.times do
-  Appointment.create!(doctor: doctors.sample, patient: patients.sample, date: Faker::Date.between(from: '2020-11-01', to: '2020-12-01'))
+  Appointment.create(doctor: doctors.sample, patient: patients.sample, date: Faker::Date.between(from: '2020-11-01', to: '2020-12-01'), city: cities.sample)
   # doctor_specialty = DoctorSpecialty.create(doctor: doctors.sample, specialty: specialties.sample)
 end
