f9ed1e4e0c99bdf72585313fa31f04cd56b26dfe
piwiit <bouillaux@hotmail.fr>
2020-10-23 00:23:32 +0200

    add seed first step

--- Gemfile
+++ Gemfile
@@ -51,4 +51,6 @@ group :test do
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
-gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+# gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+gem 'faker', :git => 'https://github.com/faker-ruby/faker.git', :branch => 'master'
+gem "table_print"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+Doctor.destroy_all
+Patient.destroy_all
+# City.destroy_all
+# Specialty.destroy_all
+
+doctors = []
+patients = []
+# cities = Array.new
+# specialties = []
+
+spes = %w[Allergist Anesthesiologist Cardiologist Dermatologist Endocrinologist Gastroenterologist Hematologist Immunologist Internist]
+
+# spes.each do |specialty|
+#   spe = Specialty.create(name: specialty)
+#   specialties << spe
+# end
+
+# 10.times do
+#   city = City.create(name: Faker::Address.city)
+#   cities << city
+# end
+
+30.times do
+  doctor = Doctor.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, zip_code: Faker::Address.zip_code, specialty: spes.sample)
+  patient =  Patient.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
+  doctors << doctor
+  patients << patient
+end
+
+30.times do
+  Appointment.create!(doctor: doctors.sample, patient: patients.sample, date: Faker::Date.between(from: '2020-11-01', to: '2020-12-01'))
+  # doctor_specialty = DoctorSpecialty.create(doctor: doctors.sample, specialty: specialties.sample)
+end
